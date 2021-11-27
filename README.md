3c9f59be51f08e42bc8dac235dcefcd6bae3a60c
Josshy <josshy92@hotmail.com>
2021-11-27 13:35:27 -0600

    added full questions and readme creator

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# ReadMe-Creator
+# Test Title 1
+
+[![License](https://img.shields.io/badge/License-Boost_1.0-blue.svg)](https://opensource.org/licenses/Boost_1.0)
+
+## Description
+ This is a test description for this app. 
+
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+The installation instructions for the app are.
+
+
+## Usage
+Some examples of use are one, two, three.
+
+
+
+## License
+ Check out the badges hosted by [shields.io](https://shields.io/). [![License](https://img.shields.io/badge/License-Boost_1.0-blue.svg)](https://opensource.org/licenses/Boost_1.0)
+
+
+
+## How to Contribute
+To do so please follow these steps.
+
+
+## Tests
+To run this application, you can open a terminal and..
+
+## Questions
+josshy92 -> "https://github.com/josshy92 
+
+josshyolea2021@u.northwestern.edu -> For further questions, please feel free to contact me @josshy92@gmail.com
+
+## Live application video here! -> https://watch.screencastify.com/v/oJC3HGAz3TUFPhoaDe10
---
+++ index.js
@@ -0,0 +1,104 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateReadme = (data) =>
+`# ${data.title}
+
+[![License](https://img.shields.io/badge/License-${data.badges}-blue.svg)](https://opensource.org/licenses/${data.badges})
+
+## Description
+ ${data.description}
+
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#badges)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+
+## Usage
+${data.usage}
+
+
+
+## Badges
+ Check out the badges hosted by [shields.io](https://shields.io/). [![License](https://img.shields.io/badge/License-${data.badges}-blue.svg)](https://opensource.org/licenses/${data.badges})
+
+
+
+## How to Contribute
+${data.contributions}
+
+
+## Tests
+${data.tests}
+
+## Questions
+${data.username} -> "https://github.com/${data.username} 
+
+${data.email} -> For further questions, please feel free to contact me @josshy92@gmail.com`;
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the Title?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Descrition:?',
+            name: 'description',
+        },
+        {
+            type: 'list',
+            message: 'Choose your license',
+            name: 'badges',
+            choices: ['MIT', 'Apache_2.0', 'Boost_1.0']
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What are some examples for use?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'If you created an app or package and would like other developers to contribute it, you can include guidelines for how to do so here.',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'Tests for the application? Please provide examples on how to run them here.',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'Github User Name:',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'Email address:',
+            name: 'email',
+        },
+    ])
+    .then((response) => {
+        console.log(response)
+        const readMePageContent = generateReadme(response)
+
+        fs.writeFile('README.md', readMePageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README file!')
+        );
+    }
+    );
